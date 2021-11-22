ea2b357b0b2295ef80340aa7ab13ad2412b41249
Mahika Kushwaha <mahikakushwaha@gmail.com>
2021-11-22 09:30:37 +0530

    updated indentation

--- public/js/main.js
+++ public/js/main.js
@@ -3,21 +3,7 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
-// Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
-});
-
 const socket = io();
-
-// Join chatroom
-socket.emit('joinRoom', { username, room });
-
-// Get room and users
-socket.on('roomUsers', ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
-});
 
 // Message from server
 socket.on('message', (message) => {
@@ -65,20 +51,6 @@ function outputMessage(message) {
   document.querySelector('.chat-messages').appendChild(div);
 }
 
-// Add room name to DOM
-function outputRoomName(room) {
-  roomName.innerText = room;
-}
-
-// Add users to DOM
-function outputUsers(users) {
-  userList.innerHTML = '';
-  users.forEach((user) => {
-    const li = document.createElement('li');
-    li.innerText = user.username;
-    userList.appendChild(li);
-  });
-}
 
 //Prompt the user before leave chat room
 document.getElementById('leave-btn').addEventListener('click', () => {
